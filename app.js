4d0be687cc2628485e007f691eb7fee22b8f71c0
andriiglukhyi <andrigluxii@gmail.com>
2017-12-07 09:50:04 -0800

    make it look pretty

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ console.log('user name',useName);
 
 var countAnsw = 0;
 
+
+
+// question 1
 var question1= prompt('Hello '+ useName + ' I are have a couple question for you! You should answer Yes or No. Ok? ');
 console.log('1st question', question1);
 
@@ -16,6 +19,10 @@ if (question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y' ) {
 if (question1.toLowerCase() === 'no' || question1.toLowerCase() === 'n' || question1.toLowerCase === 'none') {
   alert ('I have to ask you any way');
 }
+
+
+
+//question 2
 
 var language= prompt('Do you think I am bilingual?');
 console.log('bilingual', language);
@@ -29,6 +36,9 @@ if (language.toLowerCase() === 'no' || language.toLowerCase() === 'n') {
   alert('No. I am speak a couple languages');
 }
 
+
+//question 3
+
 var apart= prompt('Do I live in apartment?');
 console.log("apartment : ", apart);
 
@@ -39,6 +49,9 @@ if (apart.toUpperCase() === 'YES' || apart.toUpperCase() === 'Y') {
 if (apart.toUpperCase() === 'NO'|| apart.toUpperCase() === 'N') {
   alert ('Wrong answer');
 }
+
+
+//question 3
 
 var expir= prompt('Do you thing I have a job expirience');
 console.log('expirience', expir);
@@ -51,6 +64,8 @@ if (expir.toLowerCase() === 'yes' || expir.toLowerCase() === 'y') {
 if (expir.toLowerCase() === 'no' || expir.toLowerCase() === 'n') {
   alert('Coret answer');
 }
+
+//question 4
 
 var goal=prompt('Do you think I want to be a developer?');
 console.log('goal',goal);
@@ -68,8 +83,8 @@ alert('Wrong answer');
 var number;
 var counter = 0;
 while(number != 26) {
-  number = (prompt('how old am i?'));
   console.log('age', number)
+  number = (prompt('how old am i?'));
   if(number<26) {
     alert('Thanks. But I am older than' + number);
     counter++
